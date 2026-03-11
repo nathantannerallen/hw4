c799136a9f4c2bff10a6f21347a34ed36cef78f3
nathantannerallen <35588517+nathantannerallen@users.noreply.github.com>
2026-03-11 21:30:37 +0000

    added user auth to entry creation

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,15 +2,29 @@ class EntriesController < ApplicationController
   before_action :require_login
 
   def new
-    @entry = Entry.new(place_id: params["place_id"])
+    @place = current_user.places.find_by(id: params["place_id"])
+
+    if @place == nil
+      redirect_to "/places"
+      return
+    end
+
+    @entry = Entry.new(place_id: @place.id)
   end
 
   def create
+    @place = current_user.places.find_by(id: params["place_id"])
+
+    if @place == nil
+      redirect_to "/places"
+      return
+    end
+
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
+    @entry["place_id"] = @place.id
     @entry["user_id"] = current_user["id"]
     @entry.uploaded_image.attach(params["uploaded_image"]) if params["uploaded_image"].present?
 

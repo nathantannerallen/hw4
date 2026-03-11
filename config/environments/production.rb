bdc21f6bab18fbb734b3a5c444e5a0b96dafda81
nathantannerallen <35588517+nathantannerallen@users.noreply.github.com>
2026-03-11 21:34:47 +0000

    finishing touches on file upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  # Store uploaded files on the local filesystem in production.
+  config.active_storage.service = :local
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
